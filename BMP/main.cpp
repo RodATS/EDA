a5c30e8761e39298609133314ee1d21691bb072a
RodATS <77297145+RodATS@users.noreply.github.com>
2022-09-16 21:11:35 -0500

    Update main.cpp

--- BMP/main.cpp
+++ BMP/main.cpp
@@ -3,18 +3,19 @@ using namespace std;
 
 unsigned char* LeerBMP(char* filename)
 {
+    //Abrir el archivo
     FILE* f = fopen(filename, "rb");
 
     if(f == NULL)
         throw "Argument Exception";
 
+    //Leemos el archivo y alamacenamos los headers del bitmap
     unsigned char info[54];//windows bitmap
     fread(info, sizeof(unsigned char), 54, f);
 
     // extraer la altura y ancho header BMP
     int height = *(int*)&info[22];
     int width = *(int*)&info[18];
-    cout << endl;
 
     int row_padded = (width*3 + 3) & (~3);
     unsigned char* data = new unsigned char[row_padded];
